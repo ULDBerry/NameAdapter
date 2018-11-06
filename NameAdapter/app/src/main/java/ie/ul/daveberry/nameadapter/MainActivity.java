89af4e4c7016a214a27b0c1761fb7563576d76e6
ULDBerry <39335342+ULDBerry@users.noreply.github.com>
2018-11-06 21:16:18 +0000

    check in completed project
    following of name adapater

---
+++ NameAdapter/app/src/main/java/ie/ul/daveberry/nameadapter/MainActivity.java
@@ -0,0 +1,39 @@
+package ie.ul.daveberry.nameadapter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    RecyclerView recyclerView = findViewById(R.id.recycler_view);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    recyclerView.setHasFixedSize(true);
+    final NameAdapter nameAdapter = new NameAdapter();
+    recyclerView.setAdapter(nameAdapter);
+
+    nameAdapter.addName();
+    nameAdapter.addName();
+    nameAdapter.addName();
+
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        nameAdapter.addName();
+      }
+    });
+  }
+}
